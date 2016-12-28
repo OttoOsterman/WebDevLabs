9f9a4fd9d89748efd4c8f346a13e0c94bb069a57
OttoOsterman <ottosterman@gmail.com>
2016-12-28 20:19:40 +0200

    switch

--- switch.php
+++ switch.php
@@ -10,14 +10,14 @@
     include 'navbar.php';
     $wantedgood = "Beer";
     switch ($wantedgood) {
-        case $wantedgood = "Energy Drink":
-            echo "You have to be 15 to buy " . $wantedgood . ".";
+        case ($wantedgood = "Energy Drink"):
+            echo "You have to be 15 to buy Energy Drink";
             break;
-        case $wantedgood = "Beer":
-            echo "You have to be 18 to buy " . $wantedgood . ".";
+        case ($wantedgood = "Beer"):
+            echo "You have to be 18 to buy Beer";
             break;
-        case $wantedgood = "Spirits":
-            echo "You have to be 21 to buy " . $wantedgood . ".";
+        case ($wantedgood = "Spirits"):
+            echo "You have to be 21 to buy Spirits";
             break;
         default:
             echo "You're too young to buy anything!";
