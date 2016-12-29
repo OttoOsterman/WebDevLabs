8bccbc98e2e55e3a2cf26fc3972436a42b8718e9
OttoOsterman <ottosterman@gmail.com>
2016-12-29 16:01:27 +0200

    loops

--- loops.php
+++ loops.php
@@ -11,12 +11,15 @@
     for ($i = 1; $i < 30; $i++) {
         if ($i % 2 == 0) {
             print "Day: " . $i . "Energy Drinks available";
+            echo "<br>";
         }
         if ($i % 3 == 0) {
             print "Day: " . $i . "Beer available";
+            echo "<br>";
         }
         if ($i % 4 == 0) {
             print "Day: " . $i . "Spirits available";
+            echo "<br>";
         }
     }
     ?>
