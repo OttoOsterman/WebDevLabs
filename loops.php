b22716dc93939dc063d6439bd022d29626fe1d1e
OttoOsterman <ottosterman@gmail.com>
2016-12-29 15:55:40 +0200

    loops

--- loops.php
+++ loops.php
@@ -14,23 +14,26 @@
 //        print "<p>$x</p>";
 //    }
 
-    for ($i = 2; $i < 30; ($i+2))
+    for ($i = 1; $i < 30; $i++)
     {
-        print $i;
-        echo "<p>Energy Drinks available</p>";
+        if ($i % 2 == 0) {
+            echo "<p>Day: </p>";
+            print $i;
+            echo "<p>Energy Drinks available</p>";
+        }
     }
 
-    for ($i = 3; $i < 30; ($i+3))
-    {
-        print $i;
-        echo "<p>Beer available</p>";
-    }
-
-    for ($i = 4; $i < 30; ($i+4))
-    {
-        print $i;
-        echo "<p>Spirits available</p>";
-    }
+//    for ($i = 1; $i < 30; $i++)
+//    {
+//        print $i;
+//        echo "<p>Beer available</p>";
+//    }
+//
+//    for ($i = 1; $i < 30; $i++)
+//    {
+//        print $i;
+//        echo "<p>Spirits available</p>";
+//    }
     ?>
 </p>
 </body>
