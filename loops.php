28a965a99ab238c8c77952ee6815522f03104d55
OttoOsterman <ottosterman@gmail.com>
2016-12-29 15:46:34 +0200

    loops

--- loops.php
+++ loops.php
@@ -1,7 +1,6 @@
 <!DOCTYPE html>
 <html>
 <head>
-
 </head>
 <body>
 <p>
@@ -9,30 +8,25 @@
     <?php
     include 'navbar.php';
 
-//    $multi2;
-//    $multi3;
-//    $multi4;
+//    $drinks = array("Energy Drink", "Beer", "Spirits");
+//
+//    foreach($drinks as $x) {
+//        print "<p>$x</p>";
+//    }
 
-
-    $drinks = array("Energy Drink", "Beer", "Spirits");
-
-    foreach($drinks as $x) {
-        print "<p>$x</p>";
-    }
-
-    for ($i = 30; $i < 2; $i++)
+    for ($i = 2; $i < 30; $i++)
     {
         print $i;
         echo "<p>Energy Drinks available</p>";
     }
-    echo "<p>Energy Drinks available</p>";
-    for ($i = 30; $i < 3; $i++)
+
+    for ($i = 3; $i < 30; $i++)
     {
         print $i;
         echo "<p>Beer available</p>";
     }
 
-    for ($i = 30; $i < 4; $i++)
+    for ($i = 4; $i < 30; $i++)
     {
         print $i;
         echo "<p>Spirits available</p>";
