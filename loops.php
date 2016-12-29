33d2261a547198b37a483695c7f494f44abbba82
OttoOsterman <ottosterman@gmail.com>
2016-12-29 16:39:48 +0200

    loops

--- loops.php
+++ loops.php
@@ -13,10 +13,10 @@
         if ($i % 2 == 0) {
             echo "<br><p>Energy Drinks available</p>";
         }
-        else if ($i % 3 == 0) {
+        if ($i % 3 == 0) {
             echo "<br><p>Beer available</p>";
         }
-        else if ($i % 4 == 0) {
+        if ($i % 4 == 0) {
             echo "<br><p>Spirits available</p>";
         }
 //        if ((($i%2==0) && ($i%3==0) && ($i%4==0)) == false) {
